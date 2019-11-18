d7a16de38fc56c536be8e59889bdd68b783db550
Cody <cody@Thuy.local>
2019-11-18 18:08:15 +0700

    update redux-saga

---
+++ src/js/sagas/api-sagas.js
@@ -0,0 +1,21 @@
+import { takeEvery, call, put } from "redux-saga/effects";
+import { DATA_LOADED, DATA_REQUESTED } from "../constants/action-types";
+
+function* workerSaga() {
+  try {
+    const data = yield call(getData);
+    yield put({ type: DATA_LOADED, data });
+  } catch (e) {
+    yield put({ type: "API_ERRORED", data: e });
+  }
+}
+
+function getData() {
+  return fetch("https://jsonplaceholder.typicode.com/posts").then(response =>
+    response.json()
+  );
+}
+
+export default function* watcherSaga() {
+  yield takeEvery(DATA_REQUESTED, workerSaga);
+}
